fc962decbe6ddb9ffeea6274fb22588a15067939
Lillymuller <eleltaalemu@gmail.com>
2023-11-11 02:06:27 -0800

    YEshell_loop

---
+++ YEshell_loop.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+*main - Shell entry loop
+*@arc: argument count
+*@argv: argument variables
+*Return: Always 0
+*/
+
+int main(int arc, char **argv[])
+{
+/* function loop calls */
+	(void)arc, (void)argv;
+	void *YE_read_line(void);
+	char **par_strtok(char *);
+	int fork_exe_wait(char **);
+/* function declarations */
+	char *RD_LINE, **PARSE_ARGS;
+	int Status, indx;
+
+	while (1)
+	{
+		RD_LINE = YE_read_line();
+		if (RD_LINE == NULL)
+		{
+			perror(" Enter arguments");
+			exit(0);
+		}
+		PARSE_ARGS = par_strtok(RD_LINE);
+		indx++;
+		if (PARSE_ARGS == NULL)
+		{
+			perror("Error");
+			free(RD_LINE);
+			return (0);
+		}
+		(Status = fork_exe_wait(PARSE_ARGS));
+		indx++;
+		if ((int)Status == 0)
+		{
+			free(PARSE_ARGS);
+			free(RD_LINE);
+			return (0);
+		}
+		if (isatty(STDIN_FILENO) == -1)
+		{
+			write(1, "\n", 1);
+			return (EXIT_SUCCESS);
+		}
+		return (0);
+}
+}
+
+/**
+*RD_LINE - read input from stream
+*PARSE_ARGS - splits the input when delimi
+* is found and replaces it with \0
+*Status - executes data comparing the builtin
+*shell commands and assigned processes
+*and free the static strings
+*(RD_LINE & PARSE_ARGS)
+*/
