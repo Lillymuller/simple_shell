0d5b640307144a46e8385125981d9a68851c6f1f
Yared91 <yaredkebede010@gmail.com>
2023-11-11 03:49:12 -0800

    cdpwd.c

---
+++ cdpwd.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * YE_cd - gets the change directory process
+ * @cmd: the command input
+ * Return: Always 0
+ */
+
+void *YE_cd(char **cmd)
+{
+if (cmd[1] == NULL)
+{
+perror("Error");
+}
+else if (chdir((char *)cmd[0]) != 0)
+{
+fprintf(stderr, "Error changing directory");
+}
+return (0);
+}
+
+/**
+ * YE_pwd - gets the absolute pathname
+ * @cmd: command input
+ * Return: Always 0
+ */
+
+void *YE_pwd(char **cmd)
+{
+char AB[300];
+if (cmd[0] == NULL)
+{
+perror("Error");
+}
+else if (getcwd((char *)cmd[0], sizeof(AB) != 0))
+{
+fprintf(stderr, "Error getting absolute pathname");
+}
+return (0);
+}
