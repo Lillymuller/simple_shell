750dfb1a0fc9ff52128f8ec70b27ea1e68e0f77b
Lillymuller <eleltaalemu@gmail.com>
2023-11-15 13:26:30 -0800

    YEstrcat.c

--- YEstrcat.c
+++ YEstrcat.c
@@ -12,15 +12,20 @@ void *YE_strcat(char *field, char *tank)
 int a = 0;
 int b = 0;
 
-while (field[a] != '\0')
+while (field[a] != '\0' || tank[b] != '\0')
 {
+	if (tank[b] != '\0')
+	{
+		field[a + b] = tank[b];
+		b++;
+	}
+
+if (field[a] != '\0')
+{
+	field[a] = tank[b];
+	b++;
+}
 a++;
 }
-while (tank[b] != '\0')
-{
-field[a + b] = tank[b];
-b++;
-}
-
 return (field);
 }
