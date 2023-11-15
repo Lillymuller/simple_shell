420ac5a9dbcdc6298989b7cf942d12ef4945d667
Yared91 <yaredkebede010@gmail.com>
2023-11-15 01:32:57 -0800

    setenv.c

--- setenv.c
+++ setenv.c
@@ -11,33 +11,23 @@
 int setenv(const char *var, const char *worth, int overwrite)
 {
 char *possessed_val = getenv(var);
-char *gained_var;
-char *buff;
+char *gained_var = malloc(sizeof(strlen(var) + strlen(worth) + 4));
 
-if (possessed_val != NULL && !overwrite)
+if (possessed_val != NULL)
 {
+setenv(var, worth, overwrite);
 exit(EXIT_SUCCESS);
 }
-
-gained_var = malloc(sizeof(strlen(var) + strlen(worth) + 4));
-if (gained_var == NULL)
+else
 {
+putenv(gained_var);
+}
+sprintf(gained_var, "%s=%s", var, worth);
+
+do {
 perror("Error\n");
 exit(EXIT_FAILURE);
-}
-
-buff = malloc(sizeof(strlen(var) + strlen(worth) + 4));
-if (buff == NULL)
-{
-perror("Error\n");
-exit(EXIT_FAILURE);
-}
-
-strcpy(buff, gained_var);
-putenv(buff);
+} while (gained_var == NULL);
 
 free(gained_var);
-free(buff);
-
-return (0);
 }
