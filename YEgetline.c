780632a181095c316b83d029a69d2e2d47940e66
Lillymuller <eleltaalemu@gmail.com>
2023-11-22 10:07:53 -0800

    update

--- YEgetline.c
+++ YEgetline.c
@@ -12,11 +12,9 @@ char *YE_read_line(void)
 
 	if (getline(&RD_LINE, &memo_size, stdin) == -1)
 	free(RD_LINE);
-	return (RD_LINE);
-	RD_LINE = NULL;
+	return (NULL);
 
-	free(RD_LINE);
-	return (0);
+	return (RD_LINE);
 }
 
 /**
